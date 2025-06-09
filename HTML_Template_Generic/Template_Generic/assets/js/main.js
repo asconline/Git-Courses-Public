d86cf805d9c59202a7a23ddf07611c40e2c7ac80
Erik Bigras <eb105@queensu.ca>
2025-06-09 13:23:10 -0400

    CHanged Fall and Winter dates to 2025/26 semesters

--- HTML_Template_Generic/Template_Generic/assets/js/main.js
+++ HTML_Template_Generic/Template_Generic/assets/js/main.js
@@ -11,9 +11,9 @@ function ChangeDates () {
 
 //	this is the only area that needs to be updated
 // 	set the start dates as Monday one week prior to semester start
-var indateFall = new Date("August 26, 2024 00:00:01"); 	// input date
+var indateFall = new Date("August 25, 2025 00:00:01"); 	// input date
 var fallStart = new Date("September 3, 2024 00:00:01");  // Semester fall start date
-var indateWinter = new Date("December 30, 2024 00:00:01"); 	// input date
+var indateWinter = new Date("December 29, 2025 00:00:01"); 	// input date
 var indateSummer = new Date("April 28, 2025 00:00:01"); 	// input date
 var indateLSummer = new Date("June 23, 2025 00:00:01"); 	// input date
 
